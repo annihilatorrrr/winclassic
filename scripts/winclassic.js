9654398744401ecbee03921889dbded2be55fbdd
tPenguinLTG <tpenguinltg@disroot.org>
2025-12-16 13:01:14 -0500

    Allow downloading an entire .theme file
    Add a button to download a full .theme file containing the configured colours. A text box has also been added to accept the theme name, which is also used as the default filename.
    
    The font and other properties are currently fixed and may be made configurable in the future.
    
    Resolves #3

--- scripts/winclassic.js
+++ scripts/winclassic.js
@@ -67,6 +67,7 @@ function WinClassicTheme() {
   this.useGradientsToggle = document.getElementById("use-gradients");
   this.undoButton = document.getElementById("undo-button");
   this.redoButton = document.getElementById("redo-button");
+  this.nameInput = document.getElementById("theme-name");
 
   this.undoButton.onclick = this.undo.bind(this);
   this.undoButton.disabled = true;
@@ -84,6 +85,10 @@ function WinClassicTheme() {
 
   document.getElementById("import-action").onclick = function(e) {
     this.importIniSection(importSource.value);
+  }.bind(this);
+
+  document.getElementById("download-theme-action").onclick = function() {
+    this.downloadTheme();
   }.bind(this);
 
   this.linkElementsToggle.onchange = function() {
@@ -145,8 +150,69 @@ WinClassicTheme.prototype.exportToIni = function() {
   return ini.trim();
 }
 
+WinClassicTheme.prototype.exportToTheme = function() {
+  return ""
+    + "; Created with the Windows Classic Theme Designer: https://tpenguinltg.github.io/winclassic/\r\n"
+    + "\r\n"
+    + "\r\n"
+    + "[Theme]\r\n"
+    + "DisplayName=" + this.nameInput.value
+    + "\r\n"
+    + "\r\n"
+    + "[Control Panel\\Colors]\r\n"
+    + this.exportToIni().split("\n").join("\r\n") + "\r\n"
+    + "\r\n"
+    + "\r\n"
+    + "[Control Panel\\Desktop]\r\n"
+    + "Wallpaper=\r\n"
+    + "TileWallpaper=0\r\n"
+    + "WallpaperStyle=2\r\n"
+    + "Pattern=\r\n"
+    + "ScreenSaveActive=0\r\n"
+    + "\r\n"
+    + "[Control Panel\\Desktop\\WindowMetrics]\r\n"
+    + "\r\n"
+    + "[Metrics]\r\n"
+    + "IconMetrics=76 0 0 0 75 0 0 0 75 0 0 0 1 0 0 0 245 255 255 255 0 0 0 0 0 0 0 0 0 0 0 0 144 1 0 0 0 0 0 1 0 0 0 0 84 97 104 111 109 97 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  \r\n"
+    + "NonclientMetrics=84 1 0 0 1 0 0 0 16 0 0 0 16 0 0 0 18 0 0 0 18 0 0 0 245 255 255 255 0 0 0 0 0 0 0 0 0 0 0 0 188 2 0 0 0 0 0 1 0 0 0 0 84 97 104 111 109 97 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 12 0 0 0 15 0 0 0 245 255 255 255 0 0 0 0 0 0 0 0 0 0 0 0 188 2 0 0 0 0 0 1 0 0 0 0 84 97 104 111 109 97 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 18 0 0 0 18 0 0 0 245 255 255 255 0 0 0 0 0 0 0 0 0 0 0 0 144 1 0 0 0 0 0 1 0 0 0 0 84 97 104 111 109 97 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 245 255 255 255 0 0 0 0 0 0 0 0 0 0 0 0 144 1 0 0 0 0 0 1 0 0 0 0 84 97 104 111 109 97 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 245 255 255 255 0 0 0 0 0 0 0 0 0 0 0 0 144 1 0 0 0 0 0 1 0 0 0 0 84 97 104 111 109 97 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  \r\n"
+    + "\r\n"
+    + "[MasterThemeSelector]\r\n"
+    + "MTSM=DABJDKT\r\n"
+    + "ThemeColorBPP=4\r\n"
+    + "\r\n"
+    + "\r\n"
+    + "\r\n"
+    + "[VisualStyles]\r\n"
+    + "Path=\r\n"
+    + "ColorStyle=@themeui.dll,-854\r\n"
+    + "Size=@themeui.dll,-2019\r\n"
+    + "Transparency=0\r\n"
+    + "ColorizationColor=0XEC" + this.items["ActiveTitle"].color.slice(1).toUpperCase() + "\r\n"
+    + "Composition=0\r\n"
+    + "VisualStyleVersion=10\r\n"
+    + "\r\n"
+    + "[boot]\r\n"
+    + "SCRNSAVE.EXE=";
+}
+
 WinClassicTheme.prototype.displayExport = function() {
   this.exportDestination.value = this.exportToIni();
+}
+
+WinClassicTheme.prototype.downloadTheme = function() {
+  var uri ="data:text/plain;charset=utf-8," + encodeURIComponent(this.exportToTheme());
+
+  var element = document.createElement("a");
+  element.setAttribute("href", uri);
+  element.setAttribute("download", this.nameInput.value + ".theme");
+  element.setAttribute("target", "_blank");
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
 
 WinClassicTheme.prototype.parseIniSection = function(content) {
